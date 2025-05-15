3992e92fdeb7634aa12733d94ea5f5db0c845728
kimhojin98@naver.com <kimhojin98@naver.com>
2025-05-16 03:59:20 +0900

    Fix : ReservationAlimTalk 조회 Slice 객체로 수정

--- src/main/java/com/delete_performance_with_paging/repository/ReservationAlimTalkRepository.java
+++ src/main/java/com/delete_performance_with_paging/repository/ReservationAlimTalkRepository.java
@@ -1,8 +1,8 @@
 package com.delete_performance_with_paging.repository;
 
 import com.delete_performance_with_paging.domain.ReservationAlimTalk;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +11,9 @@ import java.time.LocalDateTime;
 
 public interface ReservationAlimTalkRepository extends JpaRepository<ReservationAlimTalk,Long> {
     @Query(value = "select r.id from ReservationAlimTalk r where r.reqDate < :reqDate")
-    Page<Long> findReservationAlimTalkByOffset(@Param("reqDate") LocalDateTime reqDate, Pageable pageable);
+    Slice<Long> findReservationAlimTalkByOffset(@Param("reqDate") LocalDateTime reqDate, Pageable pageable);
 
     @Query(value = "select r.id from ReservationAlimTalk r where (:id is null or r.id > :id) " +
             "and r.reqDate < :reqDate order by r.id")
-    Page<Long> findReservationAlimTalkByKeySet(@Param("id") Long id,@Param("reqDate") LocalDateTime reqDate,Pageable pageable);
+    Slice<Long> findReservationAlimTalkByKeySet(@Param("id") Long id, @Param("reqDate") LocalDateTime reqDate, Pageable pageable);
 }
